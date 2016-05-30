16ed7335b7961b196354ec2a0cf0b264fbca30ad
emikod@mac.com <emikod@mac.com>
2016-05-30 12:17:52 -0600

    Checkpoint 28 model: completed

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+
+  let(:post) { Post.create!(title: "New Post Title", body: "New Post body") }
+
+  describe "attributes" do
+    it "has title and body attributes" do
+      expect(post).to have_attributes(title: "New Post Title", body: "New Post body")
+    end
+  end
+end
