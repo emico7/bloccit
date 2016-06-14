63eef25c7e0051f3116e33cc3a5038e190f29ae5
emikod@mac.com <emikod@mac.com>
2016-06-14 11:04:47 -0600

    Assignment 40 moderator: in process2

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -63,8 +63,11 @@ class TopicsController < ApplicationController
   end
 
   def authorize_user
-    unless current_user.admin?
+    if current_user.admin?
       flash[:alert] = "You must be an admin to do that."
+      redirect_to topics_path
+    else current_user.moderator?
+      flash[:alert] = "You must be a moderator to do that."
       redirect_to topics_path
     end
   end
