cd9414054e7bdb41ce91eb3d6b2af5a6ef762dbb
emikod@mac.com <emikod@mac.com>
2016-06-15 15:01:45 -0600

    Assignment 42 comments: in process

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe Topic, type: :model do
 
   it { is_expected.to have_many(:labels).through(:labelings) }
 
+  it { is_expected.to have_many(:topics) }
+
 
   describe "attributes" do
     it "has name, description, and public attributes" do
