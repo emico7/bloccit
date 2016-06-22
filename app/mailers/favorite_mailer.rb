2757568c7ae501800fcfeac454a97b853361918a
emikod@mac.com <emikod@mac.com>
2016-06-22 00:31:59 -0600

    Checkpoint 44 favorites: almost done

---
+++ app/mailers/favorite_mailer.rb
@@ -0,0 +1,16 @@
+class FavoriteMailer < ApplicationMailer
+  default from: "emikod@mac.com"
+
+  def new_comment(user, post, comment)
+
+    headers["Message-ID"] = "<comments/#{comment.id}@sheltered-hollows-76187>"
+    headers["In-Reply_To"] = "<post/#{post.id}@sheltered-hollows-76187>"
+    headers["References"] = "<post/#{post.id}@sheltered-hollows-76187>"
+
+    @user = user
+    @post = post
+    @comment = comment
+
+    mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+end
