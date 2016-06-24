9fd892604c6cbe1896627cde3797f2c465638e0f
emikod@mac.com <emikod@mac.com>
2016-06-23 21:56:53 -0600

    Assignment 46 private: edited

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -27,6 +27,12 @@ RSpec.describe TopicsController, type: :controller do
 
 
     describe "GET show" do
+
+      it "redirects from private topics" do
+        get :show, {id: my_private_topic.id}
+        expect(response).to redirect_to(new_session_path)
+      end
+      
       it "returns http success" do
         get :show, {id: my_topic.id}
         expect(response).to have_http_status(:success)
@@ -177,11 +183,6 @@ RSpec.describe TopicsController, type: :controller do
 
     describe "GET show" do
 
-      it "redirects from private topics" do
-        get :show, {id: my_private_topic.id}
-        expect(response).to redirect_to(new_session_path)
-      end
-      
       it "returns http success" do
         get :show, {id: my_topic.id}
         expect(response).to have_http_status(:success)
